d8d8c40057f185466fd81dc59213de255001bb94
Avi0095 <abavishek954@gmail.com>
2025-12-02 19:40:12 +0600

    Add files via upload

---
+++ 2_encoded_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+
+# Load dataset
+dataset = pd.read_csv(r'D:\New folder\dataset_with_missing.csv')
+
+# Independent and dependent variables
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("Original X:")
+print(X)
+print(y)
+
+# Handle missing values (Age and Salary)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+print("\nX after imputation:")
+print(X)
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+X=np.array(ct.fit_transform(X))
+print("\n After OneHotEncoding:")
+print(X)
