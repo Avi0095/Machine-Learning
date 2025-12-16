8d05cbb6708c05178e179fd0e8e4629d8c2d068f
Avishek Basak <abavishek954@gmail.com>
2025-12-16 20:45:07 +0600

    Add files via upload

---
+++ 20_K_means_clustering.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset= pd.read_csv(r'D:\New folder\Mall_Customers.csv')
+x= dataset.iloc[:,[3,4]].values
+#implement elbow method
+from sklearn.cluster import KMeans
+wcss=[]
+for i in range(1,11):
+  kmeans= KMeans(n_clusters=i, init='k-means++',random_state=42)
+  kmeans.fit(x)
+  wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('the elbow method')
+plt.xlabel('number of clusters')
+plt.ylabel('wcss')
+plt.show()
+#train k-means model
+kmeans= KMeans(n_clusters=5, init='k-means++',random_state=42)
+y_kmeans=kmeans.fit_predict(x)
+print(y_kmeans)
+plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='red',label='cluster 1')
+plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='cluster 2')
+plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='cluster 3')
+plt.scatter(x[y_kmeans==3,0],x[y_kmeans==3,1],s=100,c='cyan',label='cluster 4')
+plt.scatter(x[y_kmeans==4,0],x[y_kmeans==4,1],s=100,c='magenta',label='cluster 5')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='centroids')
+plt.title('cluster of customers')
+plt.xlabel('annual income(k$)')
+plt.ylabel('spending score(1-100)')
+plt.legend()
+plt.show()
