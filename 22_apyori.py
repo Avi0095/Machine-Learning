dea80bd66cba28d83d09521af8580da54e62e0c2
Avishek Basak <abavishek954@gmail.com>
2025-12-17 16:38:44 +0600

    Add files via upload

---
+++ 22_apyori.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from apyori import apriori
+
+
+dataset= pd.read_csv(r'D:\New folder\Market_Basket_Optimisation.csv',header=None)
+transactions=[]
+for i in range(0,7501):
+  transactions.append([str(dataset.values[i,j])for j in range(0,20)])
+
+  #training aprori model in dataset
+rules=apriori(transactions=transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2,max_length=2)
+
+
+#visulasing the result
+print("\n The visualized dataset is:")
+results=list(rules)
+print(results)
+
+
+#putiing the results well organized into a pandas dataframe
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+
+
+resultsinDataFrame = pd.DataFrame(
+    inspect(results),
+    columns=['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift']
+)
+
+
+#display the result non sorted
+print("\n The results into unsorted form:")
+print(resultsinDataFrame)
+
+
+#displaying the result sorted  decedecing
+print("\n The results into sorted part(descending order):")
+print(resultsinDataFrame.nlargest(n=10,columns='Lift'))
